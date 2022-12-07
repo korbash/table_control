e4da909ba3a33c392e78797b10b7354baabdedf8
swiatique <s.vlasov@rqc.ru>
2022-12-07 15:17:45 +0300

    Add parameter thetadiv for counter function

--- mylib/motors.py
+++ mylib/motors.py
@@ -13,7 +13,7 @@ from .utilities import *
 from . import driwers as dr
 
 
-def caunter(r0=62.5, Ltorch=0.6, lw=30, rw=15, dr=1):
+def caunter(r0=62.5, Ltorch=0.6, lw=30, rw=15, dr=1, thetasdiv=1):
     def integr(y, x):
         inte = np.hstack((0, cumtrapz(y, x)))
         return inte
@@ -27,7 +27,7 @@ def caunter(r0=62.5, Ltorch=0.6, lw=30, rw=15, dr=1):
         97.9162688, 36.01154809, 22.44529847, 16.77319816, 14.08756338,
         13.28444282, 14.76885344, 19.37716274, 27.14935304, 37.4940508,
         49.76789206, 63.43683519, 78.09095269
-    ]) / 4.
+    ]) / thetasdiv
     Theta = interp1d(radius, thetas, kind='cubic')
     r = np.arange(rw, r0, dr)
     dz = Map(lambda x: 1 / float(Theta(x)), r)
@@ -366,7 +366,7 @@ class MotorSystem():
             description="Forgot motion")
         self.forgotButton.on_click(self.ForgotMotion)
 
-        self.funL_x, self.funR_x, self.xMax = caunter(lw=13, rw=10, Ltorch=1)
+        self.funL_x, self.funR_x, self.xMax = caunter(lw=13, rw=10, Ltorch=1, thetasdiv=4.5)
 
         def f(fun, xMax, x):
 
